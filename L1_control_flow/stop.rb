e281ae2c090e0fa45f2b11b89df9cbbc6086b267
Isaac Sarte <aizakku09@gmail.com>
2022-01-19 20:35:38 +0800

    Folder for 2nd Activity under lesson 1

---
+++ L1_control_flow/stop.rb
@@ -0,0 +1,15 @@
+# Question 3 - stop
+# Write a while loop that takes input from the user, performs an action,
+# and only stops when the user types "STOP". 
+# Each loop can get info from the user. 
+
+# Variable Declaration
+call = ""
+
+# While loop
+while call != "STOP" do
+    puts "What are you doing? "
+    ans = gets.chomp
+    puts "What again are you doing today? "
+    call = gets.chomp
+end
