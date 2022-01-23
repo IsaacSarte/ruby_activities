a97159a0dcd3d9809ffa7200ad98b576412dc44d
Isaac Sarte <aizakku09@gmail.com>
2022-01-23 20:29:37 +0800

    poly updated for resubmitting

--- L2_polymorphism/poly_inheritance.rb
+++ L2_polymorphism/poly_inheritance.rb
@@ -1,30 +1,28 @@
 # Polymorphism using Inheritance own example
 
 class LaptopSize
-    def initialize(model, size_inches)
-        @model = model
-        @size_inches = size_inches
-    end
-
-    def details_laptop
-        puts "This is #{@model} with #{@size_inches} inches" 
+    def size_inches
+        "Size: "
     end
 end
 
 class Odyssey < LaptopSize
-    def details_laptop
-        puts "This is #{@model} with #{@size_inches} inches"
+    def size_inches
+        super
+        print " 27 inches"
     end
 end
 
 class AOC < LaptopSize
-    def details_laptop
-        puts "This is #{@model} with #{@size_inches} inches"
+    def size_inches
+        super
+        print " 32 inches"
     end
 end
 
 puts
-samsung_odyssey = Odyssey.new("Samsung Odyssey G9", 27)
-samsung_odyssey.details_laptop
-aoc = AOC.new("AOC U series", 32)
-aoc.details_laptop
+samsung_odyssey = Odyssey.new()
+samsung_odyssey.size_inches
+puts
+aoc = AOC.new()
+aoc.size_inches
