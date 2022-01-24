9ba01f9aa3b1e626b370e0702262ba80aa49602d
Isaac Sarte <aizakku09@gmail.com>
2022-01-25 00:47:45 +0800

    updated code, screenshot, and readme

--- L3_database/README.md
+++ L3_database/README.md
@@ -3,4 +3,4 @@
 
 ### Screenshot for Database Fundamental Activity No.1: 
 
-![Data Output from pgadmin](https://scontent.fmnl4-6.fna.fbcdn.net/v/t1.15752-9/271194262_641309323980637_5387590700832488436_n.png?_nc_cat=108&ccb=1-5&_nc_sid=ae9488&_nc_eui2=AeEuYBAkTjAhzX3hba9UAb0g7LJYIOND9CTsslgg40P0JA4JWuOnAFrltAzf7iSXAvhM8nMSRW0NKFQLPTYhna4U&_nc_ohc=c-ml-GSMxRcAX84qlSr&_nc_ht=scontent.fmnl4-6.fna&oh=03_AVL_j1rj_UFuATb3i8yWz7lxuhdqqWOHGkqJaBNphmqXzw&oe=6213F28E)
+![Data Output from pgadmin](https://scontent.fmnl4-1.fna.fbcdn.net/v/t1.15752-9/271921747_460138952218370_8799599827931342125_n.png?_nc_cat=109&ccb=1-5&_nc_sid=ae9488&_nc_eui2=AeHoOUku0xGlqYOMJMdJHm3IUpvXxogAWgtSm9fGiABaC-Wa__XXuC4DkZZgKDG5D1tFT7qVQdveILgOr4lp9ZWK&_nc_ohc=UGP-G8wxsYsAX-roKDR&_nc_ht=scontent.fmnl4-1.fna&oh=03_AVIGK5XaPZ2WAbUVfpKoRyClPf_GdsucfND9Z2A7Tky2GA&oe=6212B43C)
--- L3_database/fundamentals_act_1.sql
+++ L3_database/fundamentals_act_1.sql
@@ -1,13 +1,19 @@
+-- Creating the table
 CREATE TABLE students(id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY, first_name VARCHAR(50), middle_name VARCHAR(50) DEFAULT 'Blank', last_name VARCHAR(50), age INT, location VARCHAR(100));
 
-SELECT * FROM students
+-- See the table by id number in ascending order
+SELECT * FROM students ORDER BY id;
 
-INSERT INTO students(first_name, middle_name, last_name, age, location) VALUES('Ivan', 'Ingram', 'Howard', 25, 'Bulacan')
-INSERT INTO students(first_name, last_name, age, location) VALUES('Juan', 'Cruz', 18, 'Manila')
-INSERT INTO students(first_name, last_name, age, location) VALUES('Anne', 'Wall', 20, 'Manila')
-INSERT INTO students(first_name, last_name, age, location) VALUES('Theresa', 'Joseph', 21, 'Manila')
-INSERT INTO students(first_name, last_name, age, location) VALUES('Issac', 'Gray', 19, 'Laguna')
-INSERT INTO students(first_name, last_name, age, location) VALUES('Zack', 'Matthews', 22, 'Marikina')
-INSERT INTO students(first_name, last_name, age, location) VALUES('Finn', 'Lam', 25, 'Manila')
+-- Inserting the 6 records in the table
+INSERT INTO students(first_name, last_name, age, location) VALUES('Juan', 'Cruz', 18, 'Manila');
+INSERT INTO students(first_name, last_name, age, location) VALUES('Anne', 'Wall', 20, 'Manila');
+INSERT INTO students(first_name, last_name, age, location) VALUES('Theresa', 'Joseph', 21, 'Manila');
+INSERT INTO students(first_name, last_name, age, location) VALUES('Issac', 'Gray', 19, 'Laguna');
+INSERT INTO students(first_name, last_name, age, location) VALUES('Zack', 'Matthews', 22, 'Marikina');
+INSERT INTO students(first_name, last_name, age, location) VALUES('Finn', 'Lam', 25, 'Manila');
 
-DELETE FROM students WHERE id in(Select Max(id) from students)
+-- Updating the first record in the table
+UPDATE students SET first_name = 'Ivan', middle_name = 'Ingram', last_name = 'Howard', age = 25, location = 'Bulacan' where id = 1;
+
+-- Deleting the last record in the table
+DELETE FROM students WHERE id in(Select Max(id) from students);
