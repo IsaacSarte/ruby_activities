afd10dd883043ce6cf10aa54f9d20e058f08e2ab
Isaac Sarte <aizakku09@gmail.com>
2022-01-24 21:56:54 +0800

    sql activities next

---
+++ L3_database/README.md
@@ -0,0 +1,4 @@
+# PostgreSQL v10++ : 
+## There is no auto_increment
+
+![Image](/screenshots/data_output_fundamentals_act_1.jpg)
---
+++ L3_database/fundamentals_act_1.sql
@@ -0,0 +1,10 @@
+CREATE TABLE students(id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY, first_name VARCHAR(50), middle_name VARCHAR(50) DEFAULT 'Blank', last_name VARCHAR(50), age INT, location VARCHAR(100));
+
+SELECT * FROM students
+
+INSERT INTO students(first_name, last_name, age, location) VALUES('Juan', 'Cruz', 18, 'Manila')
+INSERT INTO students(first_name, last_name, age, location) VALUES('Anne', 'Wall', 20, 'Manila')
+INSERT INTO students(first_name, last_name, age, location) VALUES('Theresa', 'Joseph', 21, 'Manila')
+INSERT INTO students(first_name, last_name, age, location) VALUES('Issac', 'Gray', 19, 'Laguna')
+INSERT INTO students(first_name, last_name, age, location) VALUES('Zack', 'Matthews', 22, 'Marikina')
+INSERT INTO students(first_name, last_name, age, location) VALUES('Finn', 'Lam', 25, 'Manila')
