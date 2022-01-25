cbab4711202d986cbccd6f97a43e4906bcf4217d
Isaac Sarte <aizakku09@gmail.com>
2022-01-25 12:56:58 +0800

    updated code

--- L3_database/fundamentals_act_1.sql
+++ L3_database/fundamentals_act_1.sql
@@ -1,5 +1,5 @@
 -- Creating the table
-CREATE TABLE students(id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY, first_name VARCHAR(50), middle_name VARCHAR(50) DEFAULT 'Blank', last_name VARCHAR(50), age INT, location VARCHAR(100));
+CREATE TABLE students(id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY, first_name VARCHAR(50) NOT NULL, middle_name VARCHAR(50) DEFAULT 'Blank', last_name VARCHAR(50) NOT NULL, age INT NOT NULL, location VARCHAR(100) NOT NULL);
 
 -- See the table by id number in ascending order
 SELECT * FROM students ORDER BY id;
@@ -13,7 +13,7 @@ INSERT INTO students(first_name, last_name, age, location) VALUES('Zack', 'Matth
 INSERT INTO students(first_name, last_name, age, location) VALUES('Finn', 'Lam', 25, 'Manila');
 
 -- Updating the first record in the table
-UPDATE students SET first_name = 'Ivan', middle_name = 'Ingram', last_name = 'Howard', age = 25, location = 'Bulacan' where id = 1;
+UPDATE students SET first_name = 'Ivan', middle_name = 'Ingram', last_name = 'Howard', age = 25, location = 'Bulacan' WHERE id = 1;
 
 -- Deleting the last record in the table
 DELETE FROM students WHERE id in(Select Max(id) from students);
